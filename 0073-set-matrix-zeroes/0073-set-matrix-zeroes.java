8cf857b9ec7560141ed9383cca551d3975743ed6
PALAK KULSHRESHTHA <167022633+palak0991@users.noreply.github.com>
2026-06-21 01:17:10 +0530

    Time: 1 ms (98.46%), Space: 47.5 MB (79.81%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,47 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean firstrowzero= false;
+        boolean firstcolzero = false;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i=0;i<m;i++){
+            if (matrix[i][0]==0){
+                firstcolzero = true;
+                break;
+            }
+        }
+        for (int i=0;i<n;i++){
+            if (matrix[0][i]==0){
+                firstrowzero = true;
+                break;
+            }
+        }
+        for(int i=1 ; i<m;i++){
+            for(int j=1; j<n;j++){
+                if (matrix[i][j]==0 ){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for (int i=1;i<m;i++){
+            for (int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if (firstrowzero) {
+            for (int j = 0; j <n ; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if(firstcolzero){
+            for(int i =0; i<m ;i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
+}
