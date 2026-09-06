7a5a789e8d3b37e16b75ae01301b0b2a4328c966
PALAK KULSHRESHTHA <167022633+palak0991@users.noreply.github.com>
2026-09-06 14:35:18 +0530

    Time: 14 ms (92.53%), Space: 51.7 MB (6.48%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gp = 0;
+        int sp =0;
+        while(gp<g.length && sp<s.length){
+            if(s[sp]>=g[gp]){
+                gp++;
+            }
+            sp++;
+        }
+        return gp;
+    }
+}
